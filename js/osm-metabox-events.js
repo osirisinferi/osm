05cf346ba727c49a1a8fdeb3dfe9b0ce95a92f99
photoweblog <photoweblog@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-12-17 06:46:43 +0000

    V4.02
    git-svn-id: https://plugins.svn.wordpress.org/osm/trunk@1788214 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/osm-metabox-events.js
+++ js/osm-metabox-events.js
@@ -21,16 +21,16 @@ var MetaboxEventhandler = {
       MapTypeField = "";
       Controls = "";
       if (document.post.osm_marker_map_type.value != "Mapnik"){
-        MapTypeField = " type=\"" + document.post.osm_marker_map_type.value + "\""; 
+        MapTypeField = " type=\"" + document.post.osm_marker_map_type.value + "\"";
       }
 
       if (document.post.osm_marker_id.value != "no"){
-        MarkerId = " post_markers=\"" + document.post.osm_marker_id.value + "\"";  
+        MarkerId = " post_markers=\"" + document.post.osm_marker_id.value + "\"";
       }
       if (document.post.osm_marker_border.value != "none"){
         BorderField = " map_border=\"thin solid "  + document.post.osm_marker_border.value+ "\"";
       }
-  
+
       if (document.post.fullscreen.checked){
         Controls = "fullscreen,";
       }
@@ -48,20 +48,20 @@ var MetaboxEventhandler = {
       else {
         ControlField ="";
       }
-      GenTxt = "[osm_map_v3 map_center=\"" + lat + "," + lon + "\" zoom=\"" + zoom + "\" width=\"100%\" height=\"450\" " + BorderField + MarkerId + MapTypeField + ControlField +"]"; 
+      GenTxt = "[osm_map_v3 map_center=\"" + lat + "," + lon + "\" zoom=\"" + zoom + "\" width=\"100%\" height=\"450\" " + BorderField + MarkerId + MapTypeField + ControlField +"]";
 
       div = document.getElementById("ShortCode_Div");
       div.innerHTML = GenTxt;
     });
   },
-	
+
 	FileSC: function osm_addFilesMapEvent(a_mapname) {
 	  a_mapname.on('click', function(evt) {
 	  var lonlat = ol.proj.transform(a_mapname.getView().getCenter(), "EPSG:3857", "EPSG:4326");
 	  var lon = lonlat[0].toFixed(4);
       var lat = lonlat[1].toFixed(4);
 	  var zoom = a_mapname.getView().getZoom();
-	
+
       var FileList_ColorField  = "";
       var FileList_TypeField   = "";
       var FileList_MapTypeField = "";
@@ -70,20 +70,20 @@ var MetaboxEventhandler = {
 		  var FileList_SelectBoxField = "";
       var DisplayName = "";
       var Controls = "";
-      var ControlField =""; 
+      var ControlField ="";
       BorderField = "";
 	    fileUrls = [];
 	    fileTitles = [];
 	    fileColors = [];
-	  
+
 	  if (document.post.osm_file_list_map_type.value != "Mapnik"){
-        FileList_MapTypeField = " type=\"" + document.post.osm_file_list_map_type.value + "\""; 
+        FileList_MapTypeField = " type=\"" + document.post.osm_file_list_map_type.value + "\"";
       }
 
       if (document.post.osm_file_border.value != "none"){
         BorderField = " map_border=\"thin solid "  + document.post.osm_file_border.value+ "\"";
       }
-  
+
       if (document.post.file_fullscreen.checked){
         Controls = "fullscreen,";
       }
@@ -95,8 +95,8 @@ var MetaboxEventhandler = {
         Controls = Controls + "mouseposition,";
       }
 			if (document.post.show_selection_box.checked){
-        FileList_SelectBoxField = " file_select_box=\"one\""; 
-      }	
+        FileList_SelectBoxField = " file_select_box=\"one\"";
+      }
       if (Controls != ""){
         Controls = Controls.substr(0, Controls.length-1);
         ControlField = " control=\"" + Controls + "\"";
@@ -104,43 +104,43 @@ var MetaboxEventhandler = {
       else {
         ControlField ="";
       }
-	  
 
-			  /** handle multiple form fields in metabox with same input (layers and their files/colors/titles - links still missing (tbc) */		
+
+			  /** handle multiple form fields in metabox with same input (layers and their files/colors/titles - links still missing (tbc) */
   	  jQuery(".osmFileName").each(function(i,e) {
   	    if (jQuery(e).val() != "") {
-	  	  fileUrls.push( jQuery(e).val()); 
-	  	}	
+	  	  fileUrls.push( jQuery(e).val());
+	  	}
   	  });
-  		
+
   	  jQuery(".osmFileTitle").each(function(i,e) {
   	    if (jQuery(e).val() != "" && fileUrls[i] != "") {
   		  fileTitles.push( jQuery(e).val());
-  		  } 
+  		  }
 				else if ((fileUrls[i] != "") && (typeof fileUrls[i] !== "undefined")) {
 				  var filename = fileUrls[i];
 					var filename = filename.replace(/^.*[\\\/]/, '')
-          fileTitles.push(filename);					
+          fileTitles.push(filename);
 				}
   	  });
-  		
+
   	  jQuery(".osmFileColor").each(function(i,e) {
   	    if (jQuery(e).val() != "" && typeof(fileUrls[i]) == "string") {
-  		  fileColors.push( jQuery(e).val()); 
+  		  fileColors.push( jQuery(e).val());
   		}
   	  });
-		
+
 	  FileList_FileField = " file_list=\"" + fileUrls.join() + "\"";
-	  FileList_ColorField = " file_color_list=\"" + fileColors.join() + "\"";   
-	  FileList_TitleField = " file_title=\"" + fileTitles.join() + "\""; 
-	  
+	  FileList_ColorField = " file_color_list=\"" + fileColors.join() + "\"";
+	  FileList_TitleField = " file_title=\"" + fileTitles.join() + "\"";
+
 	  GenTxt = "[osm_map_v3 map_center=\"" + lat + "," + lon + "\" zoom=\"" + zoom + "\" width=\"100%\" height=\"450\" " + FileList_FileField + FileList_MapTypeField + FileList_ColorField + DisplayName + ControlField + BorderField + FileList_TitleField + FileList_SelectBoxField +"]";
 
       div = document.getElementById("ShortCode_Div");
       div.innerHTML = GenTxt;
 	});
     },
-		
+
 	TaggedPostsSC: function osm_addTaggedPostsMapEvent(a_mapname) {
 	  a_mapname.on('click', function(evt) {
 	  var lonlat = ol.proj.transform(a_mapname.getView().getCenter(), "EPSG:3857", "EPSG:4326");
@@ -157,58 +157,70 @@ var MetaboxEventhandler = {
       var MapBorderField = "";
       var MarkerStyleField = "";
       var StyleColorField = "";
-      
+
       var dropdown = document.getElementById("cat");
 
-      var MarkerName    = document.post.osm_geotag_marker.value;
-      
+      var MarkerName    = document.post.tagged_marker_icon.value;
+
       if (document.post.category_parent.value != "-1"){
-        CatFilterField = " tagged_filter=\"" + document.post.category_parent.value + "\""; 
+        CatFilterField = " tagged_filter=\"" + document.post.category_parent.value + "\"";
       }
 
       if (document.post.osm_geotag_map_type.value != "Mapnik"){
-        MapTypeField = " type=\"" + document.post.osm_geotag_map_type.value + "\""; 
+        MapTypeField = " type=\"" + document.post.osm_geotag_map_type.value + "\"";
       }
 
       PostTypeField = " tagged_type=\""+document.post.osm_geotag_posttype.value+"\"";
-      if (document.post.osm_geotag_marker.value != "none"){
-        MarkerField = " marker_name=\"" + MarkerName + "\"";  
+      if (document.post.tagged_marker_icon.value != "none"){
+        MarkerField = " marker_name=\"" + MarkerName + "\"";
       }
-  
-      if (document.post.osm_geotag_map_border.value != "none"){
-        MapBorderField = " map_border=\"thin solid "  + document.post.osm_geotag_map_border.value+ "\"";
+
+      if (MarkerName == "mic_black_pinother_02.png"){
+        MapBorderField = " map_border=\"thin solid grey\"";
+        StyleColorField = " tagged_color=\"black\"";
       }
-      
-	  if (document.post.osm_geotag_marker_style.value != "standard"){
-        MarkerStyleField = " tagged_param=\""  + document.post.osm_geotag_marker_style.value+ "\"";
+      else if (MarkerName == "mic_red_pinother_02.png"){
+        MapBorderField = " map_border=\"thin solid red\"";
+        StyleColorField = " tagged_color=\"red\"";
       }
-      if (document.post.osm_geotag_marker_color.value != "none"){
-        StyleColorField = " tagged_color=\""  + document.post.osm_geotag_marker_color.value+ "\"";
+      else if (MarkerName == "mic_green_pinother_02.png"){
+        MapBorderField = " map_border=\"thin solid green\"";
+        StyleColorField = " tagged_color=\"green\"";
       }
-    
-      GenTxt = "[osm_map_v3 map_center=\"" + lat + "," + lon + "\" zoom=\"" + zoom + "\" width=\"100%\" height=\"450\" " + PostTypeField + MarkerField + MapTypeField + CatFilterField + MapBorderField + MarkerStyleField + StyleColorField + "]"; 
+      else if (MarkerName == "mic_blue_pinother_02.png"){
+        MapBorderField = " map_border=\"thin solid blue\"";
+        StyleColorField = " tagged_color=\"blue\"";
+      }
+
+
+	  if (document.post.tagged_marker_style.value != "standard"){
+        MarkerStyleField = " tagged_param=\""  + document.post.tagged_marker_style.value+ "\"";
+      }
+
+      GenTxt = "[osm_map_v3 map_center=\"" + lat + "," + lon + "\" zoom=\"" + zoom + "\" width=\"100%\" height=\"450\" " + PostTypeField + MarkerField + MapTypeField + CatFilterField + MapBorderField + MarkerStyleField + StyleColorField + "]";
 
       div = document.getElementById("ShortCode_Div");
       div.innerHTML = GenTxt;
-	});		
+	});
   },
-   
+
   AddMarker: function osm_AddMarker(a_mapname, a_post_id) {
 	  a_mapname.on('click', function(evt) {
 	  var lonlat = ol.proj.transform(evt.coordinate, "EPSG:3857", "EPSG:4326");
-	  var lon = lonlat[0].toFixed(4);
+	    var lon = lonlat[0].toFixed(4);
       var lat = lonlat[1].toFixed(4);
 
       MarkerNameField = "";
       MarkerTextField = "";
 
       MarkerId = document.post.osm_add_marker_id.value;
-      MarkerIcon = document.post.osm_add_marker_icon.value;
+      /** MarkerIcon = document.post.osm_add_marker_icon.value;**/
       MarkerName = document.post.osm_add_marker_name.value;
+      MarkerIcon = document.post.marker_icon.value;
       MarkerTextField = document.post.osm_add_marker_text.value;
       MarkerTextField = MarkerTextField.replace(/(\r\n|\n|\r)/gm, "");
       MarkerTextField = MarkerTextField.replace(/(\')/gm, "&apos;");
-      
+
       osm_ajax_object.MarkerLat = lat;
       osm_ajax_object.MarkerLon = lon;
       osm_ajax_object.MarkerId = MarkerId;
@@ -223,9 +235,17 @@ var MetaboxEventhandler = {
       div.innerHTML = GenTxt;
       div02 = document.getElementById("ShortCode_Div");
       div02.innerHTML = "";
-	});		
+
+      /** if there is already a marker, delete the layer with it **/
+      var layers = a_mapname.getLayers().getArray();
+      if (layers.length > 1){
+       a_mapname.removeLayer(layers[1])
+      }
+      MarkerIconUrl = osm_ajax_object.plugin_url + "/icons/"+MarkerIcon;
+      osm_addMarkerLayer(a_mapname,   Number(lon), Number(lat), MarkerIconUrl, -16, -41, "");
+	  });
   },
-  
+
   SetGeotag: function osm_setGeotagEvent(a_mapname, a_post_id) {
     a_mapname.on('click', function(evt) {
 	var lonlat = ol.proj.transform(evt.coordinate, "EPSG:3857", "EPSG:4326");
@@ -234,10 +254,11 @@ var MetaboxEventhandler = {
 	var zoom = a_mapname.getView().getZoom();
 
     MarkerField = "";
-    var MarkerName = document.post.osm_marker_geotag.value;
 
-    if (document.post.osm_marker_geotag.value != "none"){
-      MarkerField = " marker=\""+lat+","+lon+"\" marker_name=\"" + MarkerName + "\"";  
+    var MarkerName = document.post.geotag_marker_icon.value;
+
+    if (document.post.geotag_marker_icon.value != "none"){
+      MarkerField = " marker=\""+lat+","+lon+"\" marker_name=\"" + MarkerName + "\"";
     }
 
     osm_ajax_object.lat = lat;
@@ -254,6 +275,15 @@ var MetaboxEventhandler = {
     div.innerHTML = GenTxt;
     div02 = document.getElementById("ShortCode_Div");
     div02.innerHTML = "";
-	}); 
+
+    /** if there is already a marker, delete the layer with it **/
+    var layers = a_mapname.getLayers().getArray();
+    if (layers.length > 1){
+     a_mapname.removeLayer(layers[1])
+    }
+
+    MarkerIconUrl = osm_ajax_object.plugin_url + "/icons/"+MarkerName;
+    osm_addMarkerLayer(a_mapname,   Number(lon), Number(lat), MarkerIconUrl, -16, -41, "");
+	});
   }
 }
